f1c598bb959b36d7acecab468b0f7393a2b00b94
Dibanhi Lira Gonzalez <dibanhi_gonzalez@outlook.com>
2024-02-21 17:49:50 -0600

    Clonacion de Filas funcionando

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+//Funcion para insertar fila nueva en tabla
 function insertarFila(){
     //Identificamos la tabla
     var tabla = document.getElementById('tabla');
@@ -9,10 +10,17 @@ function insertarFila(){
     var cell3 = fila.insertCell(2);
     var cell4 = fila.insertCell(3);
     var cell5 = fila.insertCell(4);
+    var cell6 = fila.insertCell(5);
     //Colocamos Valores en las celdas
-    cell1.innerHTML = '<input type="text" placeholder="Descripcion">';
-    
+    cell1.innerHTML = '<td><input class="form-control" type="text" placeholder="Descripcion"></td>';
+    cell2.innerHTML = '<td><input class="form-control" type="text" placeholder="SKU"><td>';
+    cell3.innerHTML = '<td><input class="form-control" type="number" placeholder="1"><td>';
+    cell4.innerHTML = '<td><input class="form-control" type="number" placeholder="0.00"><td>';
+    cell5.innerHTML = '<td><input class="form-control" type="number" placeholder="0.00"><td>';
+    cell6.innerHTML = '<td><button type="button" class="btn btn-danger" onclick="eliminarFila(this)">X</button><td>';
+}
 
-    //PDT AGREGAR LAS CELDAS FALTANTES
-
+function eliminarFila(button){
+    var fila = button.parentNode.parentNode;
+    fila.parentNode.removeChild(fila);
 }
